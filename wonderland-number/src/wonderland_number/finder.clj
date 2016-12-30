b458fc33306ecbfa7c5c1d28ef76da1442d341f9
Karol Wieloch <kwieloch@gmail.com>
2016-12-30 12:41:36 +0100

    wonderland_number solved

--- wonderland-number/src/wonderland_number/finder.clj
+++ wonderland-number/src/wonderland_number/finder.clj
@@ -1,5 +1,14 @@
 (ns wonderland-number.finder)
 
 (defn wonderland-number []
-  ;; calculate me
-  42)
+  (let [
+    hasAllTheSameDigits? (fn [n1 n2]
+                          (let [s1 (set (str n1))
+                                s2 (set (str n2))]
+                            (= s1 s2)))
+    wonder? (fn [n] (every? #(hasAllTheSameDigits? n (* n %1)) [2 3 4 5 6]))
+    ]
+    (first (filter wonder? (range 100000 999999)))
+
+  )
+)
