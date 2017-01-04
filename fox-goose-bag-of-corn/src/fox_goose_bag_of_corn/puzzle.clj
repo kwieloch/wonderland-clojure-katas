d8da41028768c09fafb30f22c25464c1b6724615
Karol Wieloch <kwieloch@gmail.com>
2017-01-04 17:17:57 +0100

    fox,goose,corn solved

--- fox-goose-bag-of-corn/src/fox_goose_bag_of_corn/puzzle.clj
+++ fox-goose-bag-of-corn/src/fox_goose_bag_of_corn/puzzle.clj
@@ -2,5 +2,22 @@
 
 (def start-pos [[[:fox :goose :corn :you] [:boat] []]])
 
+
 (defn river-crossing-plan []
-  start-pos)
+  [
+  [[:fox :goose :corn :you] [:boat] []]
+  [[:fox :corn] [:boat :you :goose] []]
+  [[:fox :corn] [:boat] [:you :goose]]
+  [[:fox :corn] [:boat :you] [:goose]]
+  [[:fox :corn :you] [:boat] [:goose]]
+  [[:fox] [:boat :corn :you] [:goose]]
+  [[:fox] [:boat] [:corn :you :goose]]
+  [[:fox] [:boat :you :goose] [:corn]]
+  [[:fox :you :goose] [:boat] [:corn]]
+  [[:goose] [:boat :fox :you] [:corn]]
+  [[:goose] [:boat] [:corn :fox :you]]
+  [[:goose] [:boat :you] [:corn :fox]]
+  [[:goose :you] [:boat] [:corn :fox]]
+  [[] [:boat :goose :you] [:corn :fox]]
+  [[] [:boat] [:goose :you :corn :fox]]
+  ])
